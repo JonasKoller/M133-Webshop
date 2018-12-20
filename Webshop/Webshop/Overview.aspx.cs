046b650b91ef1217463990294c286699c00dc855
JonasKoller <jonas.koller@gmx.ch>
2018-12-20 12:21:36 +0100

    go

---
+++ Webshop/Webshop/Overview.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dorflaedali.Models;
+using Dorflaedali.Persistency;
+
+namespace Webshop
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProductsPersistency persistency = new ProductsPersistency();
+            IReadOnlyCollection<Product> products = persistency.GetAll();
+
+            items.DataSource = products;
+            items.DataBind();
+        }
+    }
+}
